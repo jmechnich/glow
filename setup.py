fe8849f87baa8cdd5b27d522cc4f15e4a88571bb
jmechnich <joerg.mechnich@gmail.com>
2017-01-02 14:31:02 +0100

    fixed config file installation and cmdline flag handling

--- setup.py
+++ setup.py
@@ -1,5 +1,27 @@
-from setuptools import setup
+# detect systemd
+def have_systemd():
+    from subprocess import check_output
 
+    def get_pid(name):
+        try:
+            return [int(i) for i in check_output(["pidof",name]).split()]
+        except:
+            return []
+    return True if 1 in get_pid("systemd") else False
+
+etc_files = []
+import os
+if os.uname()[0] == 'Linux':
+    if have_systemd():
+        print "Detected systemd"
+        etc_files.append(('/etc/systemd/system',['init/glowd.service']))
+    else:
+        print "Detected sysvinit"
+        etc_files.append(('/etc/init.d',['init/glowd']))
+    if not os.path.exists('/etc/glowd.conf'):
+        etc_files.append('/etc',['glowd.conf'])
+
+from setuptools import setup
 setup(
     name='glow',
     version='0.1.0',
@@ -14,6 +36,5 @@ setup(
     scripts=[
         'glowd',
     ],
-    data_files=[('/etc/systemd/system',['init/glowd.service']),
-                ('/etc/init.d',['init/glowd'])],
+    data_files=etc_files,
  )
